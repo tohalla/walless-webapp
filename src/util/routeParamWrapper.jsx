eea5b64376a6b6d5aaf68c5d928f6846e3934369
Touko Hallasmaa <touko.hallasmaa@outlook.com>
2017-01-21 23:27:19 +0200

    update fixes routeparamwrapper

--- src/util/routeParamWrapper.jsx
+++ src/util/routeParamWrapper.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-export default (Element, params, props) =>
+export default (Element, params, props = {}) =>
   params && params.length > 0 ?
     class RouteParamWrapper extends React.Component {
       render = () =>
         <Element {...Object.assign(props,
           params.reduce((prev, curr) =>
-            this.props.routeParams[curr] ?
-              Object.assign(prev, {[curr]: this.props.routeParams[curr]})
+            this.props.params[curr] ?
+              Object.assign(prev, {[curr]: this.props.params[curr]})
             : prev,
             {}
           ))}
